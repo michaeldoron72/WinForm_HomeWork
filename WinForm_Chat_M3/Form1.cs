8ca01db1657337d3f1125686fcae86b6176d52bf
Doron Michael <michaeldoron@ganyavne.ort.org.il>
2026-05-05 00:14:30 +0300

    final update 04.05.2026

--- WinForm_Chat_M3/Form1.cs
+++ WinForm_Chat_M3/Form1.cs
@@ -7,6 +7,7 @@ namespace WinForm_Chat_M3
 {
     public partial class Form1 : Form
     {
+        // new
         // No persistent SDK instance; create per-call to allow changing model easily
         private bool isBusy = false;
         private string currentModel = "gpt-5-mini";
